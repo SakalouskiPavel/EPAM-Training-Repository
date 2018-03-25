425347e56c6456047d7a65c5452128b4faada889
Sokolovsky Pavel <p.sokolovsky62@gmail.com>
2018-03-25 18:35:53 +0300

    + day 5 task 2

---
+++ NET.W.2018.Соколовский.05/NET.W.2018.Соколовский.05.Tests/BubbleSortingTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace NET.W._2018.Соколовский._05.Tests
+{
+    [TestFixture]
+    public class BubbleSortingTests
+    {
+        [TestCase(new int[]{ 3, 2, 1 }, new int[]{ 3, 4 }, new int[]{ 5, 7, 4, 12 })]
+        public void BubbleSorting_ValidDataSumFuncAscendingFalse(params int[][] args)
+        {
+            BubbleSorting.BubbleSort(args, (x) => x.Sum(), false);
+            var result = new int[][] {new int[] {4, 5, 7, 12}, new int[] {3, 4}, new int[] {1, 2, 3}};
+            CollectionAssert.AreEqual(args, result);
+        }
+
+        [TestCase(new int[] { 3, 2, 1 }, new int[] { 3, 4 }, new int[] { 5, 7, 4, 12 })]
+        public void BubbleSorting_ValidDataSumFuncAscendingTrue(params int[][] args)
+        {
+            BubbleSorting.BubbleSort(args, (x) => x.Sum(), true);
+            var result = new int[][] { new int[] { 1, 2, 3 }, new int[] { 3, 4 }, new int[] { 4, 5, 7, 12 } };
+            CollectionAssert.AreEqual(args, result);
+        }
+
+        [TestCase(new int[] { 3, 2, 1 }, new int[] { 3, 4 }, new int[] { 5, 7, 4, 12 })]
+        public void BubbleSorting_ValidDataMaxFuncAscendingFalse(params int[][] args)
+        {
+            BubbleSorting.BubbleSort(args, (x) => x.Max(), false);
+            var result = new int[][] { new int[] { 4, 5, 7, 12 }, new int[] { 3, 4 }, new int[] { 1, 2, 3 } };
+            CollectionAssert.AreEqual(args, result);
+        }
+
+        [TestCase(new int[] { 3, 2, 1 }, new int[] { 3, 4 }, new int[] { 5, 7, 4, 12 })]
+        public void BubbleSorting_ValidDataMaxFuncAscendingTrue(params int[][] args)
+        {
+            BubbleSorting.BubbleSort(args, (x) => x.Max(), true); 
+            var result = new int[][] { new int[] { 1, 2, 3 }, new int[] { 3, 4 }, new int[] { 4, 5, 7, 12 } };
+            CollectionAssert.AreEqual(args, result);
+        }
+
+        [TestCase(new int[] { 3, 2, 1 }, new int[] { 3, 4 }, new int[] { 5, 7, 4, 12 })]
+        public void BubbleSorting_ValidDataMinFuncAscendingFalse(params int[][] args)
+        {
+            BubbleSorting.BubbleSort(args, (x) => x.Min(), false);
+            var result = new int[][] { new int[] { 4, 5, 7, 12 }, new int[] { 3, 4 }, new int[] { 1, 2, 3 } };
+            CollectionAssert.AreEqual(args, result);
+        }
+
+        [TestCase(new int[] { 3, 2, 1 }, new int[] { 3, 4 }, new int[] { 5, 7, 4, 12 })]
+        public void BubbleSorting_ValidDataMinFuncAscendingTrue(params int[][] args)
+        {
+            BubbleSorting.BubbleSort(args, (x) => x.Min(), true); 
+            var result = new int[][] { new int[] { 1, 2, 3 }, new int[] { 3, 4 }, new int[] { 4, 5, 7, 12 } };
+            CollectionAssert.AreEqual(args, result);
+        }
+    }
+}
---
+++ NET.W.2018.Соколовский.05/NET.W.2018.Соколовский.05/BubbleSorting.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace NET.W._2018.Соколовский._05
+{
+    public static class BubbleSorting
+    {
+        public static void BubbleSort(int[][] inputArrays, Func<int[], int> function, bool ascending = true)
+        {
+            if (ReferenceEquals(inputArrays, null) || inputArrays.Length == 0)
+            {
+                throw new ArgumentNullException();
+            }
+
+            foreach (var array in inputArrays)
+            {
+                if (ReferenceEquals(array, null) || array.Length == 0)
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+
+            for (int i = 0; i < inputArrays.Length; i++)
+            {
+                for (int j = 0; j < inputArrays.Length - i - 1; j++)
+                {
+                    if (ascending)
+                    {
+                        if (function(inputArrays[j]) > function(inputArrays[j + 1]))
+                        {
+                            int[] temp = inputArrays[j];
+                            inputArrays[j] = inputArrays[j + 1];
+                            inputArrays[j + 1] = temp;
+                        }
+                    }
+                    else
+                    {
+                        if (function(inputArrays[j]) < function(inputArrays[j + 1]))
+                        {
+                            int[] temp = inputArrays[j];
+                            inputArrays[j] = inputArrays[j + 1];
+                            inputArrays[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+
+            foreach (var array in inputArrays)
+            {
+                BubbleSort(array);
+            }
+        }
+
+        private static void BubbleSort(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = 0; j < inputArray.Length - i - 1; j++)
+                {
+                    if (inputArray[j] > inputArray[j + 1])
+                    {
+                        int temp = inputArray[j];
+                        inputArray[j] = inputArray[j + 1];
+                        inputArray[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
