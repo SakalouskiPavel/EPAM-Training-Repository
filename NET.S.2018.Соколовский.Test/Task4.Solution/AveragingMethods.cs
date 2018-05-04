5863d26a9cad22dabd82c55a2bc3992b018acbc4
Sokolovsky Pavel <p.sokolovsky62@gmail.com>
2018-05-04 10:25:32 +0300

    + task 4

---
+++ NET.S.2018.Соколовский.Test/Task4.Solution/AveragingMethods.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Task4
+{
+    public static class AveragingMethods
+    {
+        public static double MedianMethod(List<double> values)
+        {
+            var sortedValues = values.OrderBy(x => x).ToList();
+
+            int n = sortedValues.Count;
+
+            if (n % 2 == 1)
+            {
+                return sortedValues[(n - 1) / 2];
+            }
+
+            return (sortedValues[sortedValues.Count / 2 - 1] + sortedValues[n / 2]) / 2;
+        }
+
+        public static double MeanMethod(List<double> values)
+        {
+            return values.Sum() / values.Count;
+        }
+    }
+}
---
+++ NET.S.2018.Соколовский.Test/Task4.Solution/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task4
+{
+    public class Calculator
+    {
+        public Func<List<double>, double> Method;
+
+        public Calculator()
+        {
+            Method = AveragingMethods.MeanMethod;
+        }
+
+        public Calculator(Func<List<double>, double> method)
+        {
+            Method = method;
+        }
+
+        public double CalculateAverage(List<double> values)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            if (ReferenceEquals(Method, null))
+            {
+                throw new ArgumentException("Invalid averagingMethod value");
+            }
+
+            return Method.Invoke(values);
+        }
+    }
+}
