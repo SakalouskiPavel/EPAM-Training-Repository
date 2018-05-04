39800d1e32a99cff5b178f66fcb3f2c13b234f25
Sokolovsky Pavel <p.sokolovsky62@gmail.com>
2018-05-04 11:47:03 +0300

    + task 6

---
+++ NET.S.2018.Соколовский.Test/Task6.Tests/CustomEnumerableTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Test6.Solution;
+
+namespace Task6.Tests
+{
+    [TestFixture]
+    public class CustomEnumerableTests
+    {
+        [Test]
+        public void Generator_ForSequence1()
+        {
+            int[] expected = { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+            EnumerableGenerator<int> intEnumerableGenerator = new EnumerableGenerator<int>();
+
+            CollectionAssert.AreEqual(expected, intEnumerableGenerator.GetEnumerable(1, 1, 10, (cur, prev) => cur + prev).ToArray());
+        }
+
+        [Test]
+        public void Generator_ForSequence2()
+        {
+            int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
+            EnumerableGenerator<int> intEnumerableGenerator = new EnumerableGenerator<int>();
+
+            CollectionAssert.AreEqual(expected, intEnumerableGenerator.GetEnumerable(1, 2, 10, (cur, prev) => 6 * cur - 8 * prev).ToArray());
+
+        }
+
+        [Test]
+        public void Generator_ForSequence3()
+        {
+            double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
+            EnumerableGenerator<double> doubleEnumerableGenerator = new EnumerableGenerator<double>();
+            var result = doubleEnumerableGenerator.GetEnumerable(1, 2 , 10 , (cur, prev) => cur + prev / cur).ToArray();
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i], 0.0000001);
+            }
+        }
+    }
+}
---
+++ NET.S.2018.Соколовский.Test/Test6.Solution/EnumerableGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test6.Solution
+{
+    public class EnumerableGenerator<T>
+    {
+        public IEnumerable<T> GetEnumerable(T firstMember, T secondMember, int count, Func<T, T, T> formula)
+        {
+            yield return firstMember;
+            yield return secondMember;
+            var currentMember = secondMember;
+            var prevMember = firstMember;
+
+            for (int i = 0; i < count - 2; i++)
+            {
+                var result = formula.Invoke(currentMember, prevMember);
+                prevMember = currentMember;
+                currentMember = result;
+                yield return currentMember;
+            }
+        }
+    }
+}
